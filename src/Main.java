48527e29e3f48c00f1914657a041d0b4a0b0c00e
Braulio R. Bracamonte Salgado <brbr@ha.lan>
2019-06-07 15:19:20 -0500

    done

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import facebook4j.*;
 import facebook4j.FacebookFactory;
 import facebook4j.auth.AccessToken;
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 //import facebook4j.internal.logging.Logger;
 import java.util.logging.*;
 //import org.apache.logging.log4j.LogManager;
@@ -47,13 +49,72 @@ public class Main {
     logger.addHandler(fileHandler);
     consoleHandler.setLevel(Level.ALL);
     fileHandler.setLevel(Level.ALL);
+    int opt = 1;
     
+    System.out.format("Facebook command line %s\n\n", APP_VERSION) ;
+    //Menu
+    while(opt != 0 ){
+      //Imprimir menu
+      System.out.println("opciones:");
+      System.out.println("1-> opt uno");
+      System.out.println("2-> opt dos");
+      
+      //get option
+      opt = getUsrIn(2);
+      System.out.println(opt);
+      
+      //switch
+      switch (opt){
+        case 1:
+          System.out.println("Uno!");
+          break;
+        
+        case 2:
+          System.out.println("Dos!");
+          
+      }
+      
+      opt = 0;
+    }
     
+    enterp();
     
     logger.log(Level.INFO, "Fin del programa");
-    System.out.println("Program END");
+    //System.out.println("Program END");
+    }
+  
+    /**
+    * obtener la entrada del usario limitando su respuesta
+    * a un numero mayor que cero pero menor que @param n
+    * 
+    * @param n int numero de opciones disponibles
+    * @return     * 
+     */
+    public static int getUsrIn(int n){
+      Scanner in = new Scanner(System.in);
+      int inp = 0;
+      while(inp == 0){
+      System.out.println("Ingresa un número mayor que 0 y menor que " + Integer.toString(n + 1) );
+        try{
+          inp = in.nextInt();
+        }catch(InputMismatchException e ){
+          System.out.println("Debes ingresar un número mayor que 0 y menor que " + Integer.toString(n + 1) );
+          in.next();
+          inp = 0;
+        }
+        if(inp < 0|| inp > n ){ inp = 0; }
+      }
+      return inp;
     }
 
-    
+    /**
+     * pausar hasta que se presione "Enter"
+     */
+    public static void enterp(){
+      System.out.println("Press Enter to continue");
+      try{System.in.read();}
+      catch(Exception e){}
+    }
+  
   
 }
